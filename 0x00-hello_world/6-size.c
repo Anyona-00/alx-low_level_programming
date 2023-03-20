db269625b4e3d592e54851d064c1d22594b9985e
Anyona-00 <ombasaanyona>
2023-03-20 14:59:29 -0700

    t6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,17 +1,11 @@
 #include <stdio.h>
 /**
- * main-  prints the size of various types on the computer it is compiled and run on
- *code by Anyona
- * Return:0
+ * main -Entry point
+ *
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float f;
-
 	printf("Size of a char: %d byte(S)\n", (sizeof(char));
 	printf("Size of a int: %d byte(S)\n", sizeof(int));
 	printf("Size of a long int: %d byte(S)\n", sizeof(long int));
