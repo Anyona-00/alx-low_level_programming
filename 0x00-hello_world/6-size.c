b4bd61ef9a915f9f8a00996e01aa91bc6e032dc4
Anyona-00 <ombasaanyona>
2023-03-20 16:04:51 -0700

    t6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,13 +6,10 @@
  */
 int main(void)
 {
-    printf("The size of char is %ld bytes\n", sizeof(char));
-    printf("The size of short is %ld bytes\n", sizeof(short));
-    printf("The size of int is %ld bytes\n", sizeof(int));
-    printf("The size of long is %ld bytes\n", sizeof(long));
-    printf("The size of long long is %ld bytes\n", sizeof(long long));
-    printf("The size of float is %ld bytes\n", sizeof(float));
-    printf("The size of double is %ld bytes\n", sizeof(double));
-    printf("The size of long double is %ld bytes\n", sizeof(long double));
+    printf("size of a char: %d bytes(s)\n", sizeof(char));
+    printf("size of an int: %d bytes(s)\n", sizeof(int));
+    printf("size of long int: %d bytes(s)\n", sizeof(long int));
+    printf("size of long long int: %d bytes(s)\n", sizeof(long long int));
+    printf("size of a float: %d bytes(s)\n", sizeof(float));
     return (0);
 }
